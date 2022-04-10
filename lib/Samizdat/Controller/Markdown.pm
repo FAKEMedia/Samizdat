607556f2628711a6a62bf4cd182c33c4485178b0
Hans Svensson <hans@rymdweb.com>
2022-04-11 00:45:04 +0200

    First commit.

--- lib/Samizdat/Controller/Markdown.pm
+++ lib/Samizdat/Controller/Markdown.pm
@@ -3,7 +3,10 @@ use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 sub geturi ($self) {
   my $docpath = $self->stash('docpath');
-  my $web = {};
+  my $web = {
+    sidepanels => [],
+    main       => '',
+  };
   $self->stash(web => $web);
   $web->{main} = $docpath;
   $self->render();
--- lib/Samizdat/Plugin/Cache.pm
+++ lib/Samizdat/Plugin/Cache.pm
@@ -9,6 +9,14 @@ sub register {
   my $self = shift;
   my $app = shift;
 
+
+  $app->helper(
+    testhelper => sub {
+      my $c = shift;
+      return 1;
+    },
+  );
+
   $app->hook(
     after_render => sub {
       my ($c, $output, $format) = @_;
