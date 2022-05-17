8e2d9016cc90d29fc960180fc0fbb89d6176746c
Hans Svensson <hans@rymdweb.com>
2022-05-18 00:52:44 +0200

    First commit.

--- lib/Samizdat/Command/makeharvest.pm
+++ lib/Samizdat/Command/makeharvest.pm
@@ -1,17 +1,19 @@
 package Samizdat::Command::makeharvest;
+
 use Mojo::Base 'Mojolicious::Command', -signatures;
 use Mojo::Home;
 use Hash::Merge qw( merge );
 use YAML::XS qw(Load);
-Hash::Merge::set_behavior('RETAINMENT_PRECEDENT');
 use WWW::YouTube::Download;
 use Data::Dumper;
 
 has description => 'Fetches content from configured sources and stores locally.';
 has usage => sub ($self) { $self->extract_usage };
 
+Hash::Merge::set_behavior('RETAINMENT_PRECEDENT');
+
 sub run ($self, @args) {
-  my $path = Mojo::Home->new('sources.d/');
+  my $path = Mojo::Home->new('public/');
   my $sources = {};
   $path->list_tree({dir => 0})->each(sub ($file, $num) {
       if ('yml' eq $file->path->extname()) {
